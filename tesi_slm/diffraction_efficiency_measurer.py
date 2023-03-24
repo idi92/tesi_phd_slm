400ee5c010ffe8352f580581b8cf33cf7c02e8fc
idi92 <edoardo.bellone@inaf.it>
2023-03-24 12:43:38 +0100

    Changes on dem, but there's still a bug

--- tesi_slm/diffraction_efficiency_measurer.py
+++ tesi_slm/diffraction_efficiency_measurer.py
@@ -14,18 +14,19 @@ class DiffractionEfficiencyMeasurer():
          CameraMastersAnalyzer.load_camera_masters(fname_masters)
     
     def measure_diffraction_efficiency(self, c_span, j = 2, N_iter = 10):
-        Nframes = 5
+        Nframes = 30
         self._c_span = c_span 
         self._spoc._cam.setExposureTime(self._texp)
         
-        self._spoc.set_slm_flat()
-        flat_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
-        clean_flat = clean_cube_images(flat_cube, self._master_dark, self._master_background) 
-        yf, xf = get_index_from_image(clean_flat, value = clean_flat.max())
-        cut_clean_flat = cut_image_around_coord(clean_flat, yf, xf, halfside=25)
-        I_total = cut_clean_flat.sum()
-        
-        
+        # self._spoc.set_slm_flat()
+        # flat_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
+        # clean_flat = clean_cube_images(flat_cube, self._master_dark, self._master_background) 
+        # yf, xf = get_index_from_image(clean_flat, value = clean_flat.max())
+        # cut_clean_flat = cut_image_around_coord(clean_flat, yf, xf, halfside=25)
+        # I_total = cut_clean_flat.sum()
+        # print('I_tot:%g'%I_total)
+        #
+
         N_amp = len(c_span)
         I_mod_values = np.zeros((N_amp, N_iter))
         I_ghost_values = np.zeros((N_amp, N_iter))
@@ -36,13 +37,24 @@ class DiffractionEfficiencyMeasurer():
         
         coeff2apply = np.zeros(2)
         tilt_idx = j - 2
-        for c_idx, amp in enumerate(c_span):
+        
+        for c_idx in range(N_amp):
             
-            coeff2apply[tilt_idx] = amp
-            
+            coeff2apply[tilt_idx] = c_span[c_idx]
+            print('c = %g m'% c_span[c_idx])
             for i in range(N_iter):
+                print('%d iter'%i)
                 
                 self._spoc.set_slm_flat()
+                
+                flat_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
+                clean_flat = clean_cube_images(flat_cube, self._master_dark, self._master_background) 
+                if i == 0:
+                    yf, xf = get_index_from_image(clean_flat, value = clean_flat.max())
+                    
+                cut_clean_flat = cut_image_around_coord(clean_flat, yf, xf, halfside=25)
+                I_total = cut_clean_flat.sum()
+                print('I_tot:%g'%I_total)
                 
                 self._spoc._write_zernike_on_slm(coeff2apply)
                 
@@ -50,22 +62,26 @@ class DiffractionEfficiencyMeasurer():
                 clean_tilt = clean_cube_images(tilt_cube, self._master_dark, self._master_background)
                 if i == 0:                    
                     ym, xm = get_index_from_image(clean_tilt, value = clean_tilt.max())
+                    
                 cut_clean_tilt = cut_image_around_coord(clean_tilt, ym, xm, halfside=25)
                 cut_clean_ghost = cut_image_around_coord(clean_tilt, yf, xf, halfside=25)
                 
-                I_mod_values[c_idx, i] = cut_clean_tilt.sum()/I_total
-                I_ghost_values[c_idx, i] = cut_clean_ghost.sum()/I_total
-        self._mean_mod_ratio[c_idx] =I_mod_values[c_idx].mean()
-        self._err_mod_ratio[c_idx] =I_mod_values[c_idx].std()
-        self._mean_ghost_ratio[c_idx] =I_ghost_values[c_idx].mean()
-        self._err_ghost_ratio[c_idx] =I_ghost_values[c_idx].std()
+                I_mod_values[c_idx, i] = (cut_clean_tilt.sum())/I_total
+                I_ghost_values[c_idx, i] = (cut_clean_ghost.sum())/I_total
+                print('mod_ratiot:%g'%I_mod_values[c_idx, i])
+                print('ghost_ratio:%g'%I_ghost_values[c_idx, i])
+                
+            self._mean_mod_ratio[c_idx] = I_mod_values[c_idx,:].mean()
+            self._err_mod_ratio[c_idx] = I_mod_values[c_idx,:].std()
+            self._mean_ghost_ratio[c_idx] = I_ghost_values[c_idx,:].mean()
+            self._err_ghost_ratio[c_idx] = I_ghost_values[c_idx,:].std()
         
     def show_ratios(self):
         import matplotlib.pyplot as plt
         plt.subplots(2,1,sharex=True)
         plt.subplot(2,1,1)
-        plt.plot(self._c_span,self._mean_ghost_ratio, 'bo-', label = 'ghost ratio')
-        plt.errorbar(self._c_span,self._mean_ghost_ratio, self._err_ghost_ratio, fmt='.b')
+        plt.plot(self._c_span, self._mean_ghost_ratio, 'bo-', label = 'ghost ratio')
+        plt.errorbar(self._c_span, self._mean_ghost_ratio, self._err_ghost_ratio, fmt='.b')
         plt.xlabel('c [m]')
         plt.ylabel('$<I_{ghost}> / <I_{flat}>$')
         plt.legend(loc='best')

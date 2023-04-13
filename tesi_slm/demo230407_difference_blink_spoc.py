255e7d29bef7e5acb68bc4de915b722e42b3cd09
idi92 <edoardo.bellone@inaf.it>
2023-04-13 18:38:00 +0200

    demo added for zernike modes difference between blink and arte

--- tesi_slm/demo230407_difference_blink_spoc.py
+++ tesi_slm/demo230407_difference_blink_spoc.py
@@ -15,6 +15,11 @@ def main(fname_blink, fname_spoc):
     pippo = fits.open(fname_spoc)
     z6_spoc = pippo[0].data
     
+    cmask_obj = CircularMask(frameShape=(1152,1920),maskCenter=(550,853),maskRadius=569)
+    zg = zernike_generator.ZernikeGenerator(cmask_obj)
+    z6 = zg.getZernike(6) * 2.e-6
+    
+    
     plt.subplots(2,1,sharex=True, sharey=True)
     plt.subplot(2,1,1)
     plt.imshow(z6_blink, cmap='gray')
@@ -26,8 +31,9 @@ def main(fname_blink, fname_spoc):
     plt.figure()
     plt.plot(z6_blink[550,:], 'b-',label='Blink')
     plt.plot(z6_spoc[550,:], 'r-',label='spoc')
+    plt.plot((z6[550,:]/635e-9*256)%256, 'm--',label='unrounded')
     plt.legend(loc='best')
-    plt.vlines(853,0, 255,'k','--')
+    plt.vlines(853,0, 256,'k','--')
     plt.xlabel('pixel along x-axis')
     plt.ylabel('gray value')
     plt.grid('--', alpha=0.3)
@@ -35,48 +41,46 @@ def main(fname_blink, fname_spoc):
     plt.figure()
     plt.plot(z6_blink[:, 853], 'b-',label='Blink')
     plt.plot(z6_spoc[:, 853], 'r-',label='spoc')
+    plt.plot((z6[:,853]/635e-9*256)%256, 'm--',label='unrounded')
     plt.legend(loc='best')
-    plt.vlines(550, 0, 255,'k','--')
+    plt.vlines(550, 0, 256,'k','--')
     plt.xlabel('pixel along y -axis')
     plt.ylabel('gray value')
     plt.grid('--', alpha=0.3)
     
+
     cmask_obj = CircularMask(frameShape=(1152,1920),maskCenter=(550,853),maskRadius=569)
     zg = zernike_generator.ZernikeGenerator(cmask_obj)
     z6 = zg.getZernike(6) * 2.e-6
     
     
-    plt.subplots(2,1)
-    plt.subplot(2,1,1)
-    plt.plot(z6_blink[550,:], 'b-',label='Blink')
-    plt.plot(z6_spoc[550,:], 'r-',label='spoc')
+    plt.figure()
+    plt.plot(z6_blink[550,:], 'b.-',label='Blink')
+    plt.plot(z6_spoc[550,:], 'r.-',label='spoc')
+    plt.plot((z6[550,:]/635e-9*256)%256, 'm.--',label='unrounded')
     plt.legend(loc='best')
-    plt.vlines(853,0, 255,'k','--')
+    plt.vlines(853,0, 256,'k','--')
     plt.xlabel('pixel along x-axis')
     plt.ylabel('gray value')
     plt.grid('--', alpha=0.3)
     
-    plt.subplot(2,1,2)
-    plt.plot(z6[550,:], 'g-',label='arte')
+    plt.figure()
+    plt.plot(z6_blink[:, 853], 'b.-',label='Blink')
+    plt.plot(z6_spoc[:, 853], 'r.-',label='spoc')
+    plt.plot((z6[:,853]/635e-9*256)%256, 'm.--',label='unrounded')
     plt.legend(loc='best')
-    plt.vlines(853,0, 255,'k','--')
-    plt.xlabel('pixel along x-axis')
-    plt.ylabel('surface')
-    plt.grid('--', alpha=0.3)
-    
-    plt.subplots(2,1)
-    plt.subplot(2,1,1)
-    plt.plot(z6_blink[:, 853], 'b-',label='Blink')
-    plt.plot(z6_spoc[:, 853], 'r-',label='spoc')
-    plt.legend(loc='best')
-    plt.vlines(550, 0, 255,'k','--')
+    plt.vlines(550, 0, 256,'k','--')
     plt.xlabel('pixel along y -axis')
     plt.ylabel('gray value')
     plt.grid('--', alpha=0.3)
-    plt.subplot(2,1,2)
-    plt.plot(z6[:, 853], 'g-',label='arte')
-    plt.legend(loc='best')
-    plt.vlines(550,0, 255,'k','--')
-    plt.xlabel('pixel along y-axis')
-    plt.ylabel('surface')
-    plt.grid('--', alpha=0.3)
+    
+    z_un = (z6/635e-9*256)%256
+    print('value around center along x axis:')
+    for idx in range(834, 866):
+        print('index {}\t unrounded: {}\t blink: {}\t spoc: {}'.format(
+            idx-853, z_un[550,idx], z6_blink[550, idx], z6_spoc[550, idx]))
+        
+    print('value around center along y axis:')
+    for idx in range(539, 561):
+        print('index {}\t unrounded: {}\t blink: {}\t spoc: {}'.format(
+            idx-550, z_un[idx, 853], z6_blink[idx, 853], z6_spoc[idx, 853]))
--- tesi_slm/diffraction_efficiency_measurer.py
+++ tesi_slm/diffraction_efficiency_measurer.py
@@ -14,9 +14,12 @@ class DiffractionEfficiencyMeasurer():
          CameraMastersAnalyzer.load_camera_masters(fname_masters)
     
     def measure_diffraction_efficiency(self, c_span, texp, j = 2, N_iter = 10, half_side=25, init_coeff=None):
-        Nframes = 100
+        Nframes = 50
+        self._Nframes = Nframes
         self._c_span = c_span
         self._texp = texp
+        self._Niter = N_iter
+        self._j_noll = j
         if(self._texp != self._texp_master):
             print('WARNING: the selected exposure time (t_exp = %g ms) is different from the '\
                   'one used to measure dark and background (t_m = %g ms)\n'\
@@ -45,6 +48,8 @@ class DiffractionEfficiencyMeasurer():
         
         
         coeff2apply = self._init_coeff.copy()
+        flatcoeff2apply = self._init_coeff.copy()
+        flatcoeff2apply[0] = 0
         tilt_idx = j - 2
         
         for c_idx in range(N_amp):
@@ -54,31 +59,29 @@ class DiffractionEfficiencyMeasurer():
             for i in range(N_iter):
                 print('%d iter'%i)
                 
-                self._spoc.set_slm_flat()
-                
+                self._spoc.write_zernike_on_slm(flatcoeff2apply)
                 flat_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
-                clean_flat = clean_cube_images(flat_cube, self._master_dark, self._master_background) 
+                self._spoc.write_zernike_on_slm(coeff2apply)
+                tilt_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
+                
+                clean_flat = clean_cube_images(flat_cube, self._master_dark, self._master_background)
+                clean_tilt = clean_cube_images(tilt_cube, self._master_dark, self._master_background) 
+                
                 if i == 0:
                     yf, xf = get_index_from_image(clean_flat, value = clean_flat.max())
-                    
-                cut_clean_flat = cut_image_around_coord(clean_flat, yf, xf, halfside=half_side)
-                I_total = cut_clean_flat.sum()
-                print('I_tot:%g'%I_total)
-                
-                self._spoc.write_zernike_on_slm(coeff2apply)
-                
-                tilt_cube = self._spoc._cam.getFutureFrames(Nframes).toNumpyArray()
-                clean_tilt = clean_cube_images(tilt_cube, self._master_dark, self._master_background)
-                if i == 0:                    
                     ym, xm = get_index_from_image(clean_tilt, value = clean_tilt.max())
                     
+                cut_clean_flat = cut_image_around_coord(clean_flat, yf, xf, halfside=half_side)
                 cut_clean_tilt = cut_image_around_coord(clean_tilt, ym, xm, halfside=half_side)
                 cut_clean_ghost = cut_image_around_coord(clean_tilt, yf, xf, halfside=half_side)
-                
+                I_total = cut_clean_flat.sum()
+                print('I_tot:%g'%I_total)
                 I_mod_values[c_idx, i] = (cut_clean_tilt.sum())/I_total
                 I_ghost_values[c_idx, i] = (cut_clean_ghost.sum())/I_total
+                mod_ghost_ratio = I_mod_values[c_idx, i]/I_ghost_values[c_idx, i]
                 print('mod_ratiot:%g'%I_mod_values[c_idx, i])
                 print('ghost_ratio:%g'%I_ghost_values[c_idx, i])
+                print('mod/ghost: %g'% mod_ghost_ratio)
                 
             self._mean_mod_ratio[c_idx] = I_mod_values[c_idx,:].mean()
             self._err_mod_ratio[c_idx] = I_mod_values[c_idx,:].std()
@@ -102,4 +105,17 @@ class DiffractionEfficiencyMeasurer():
         plt.ylabel('$<I_{modulated}> / <I_{flat}>$')
         plt.legend(loc='best')
         plt.grid(ls ='--', alpha = 0.5)
-          
+    
+    def save(self, fname):
+        hdr = fits.Header()
+        hdr['T_EX_MS'] = self._texp
+        hdr['N_AV_FR'] = self._Nframes
+        hdr['N_ITER'] = self._Niter
+        hdr['Z_J'] = self._j_noll
+         
+        fits.writeto(fname, self._mean_mod_ratio,hdr)
+        
+        fits.append(fname, self._err_mod_ratio)
+        fits.append(fname, self._mean_ghost_ratio)
+        fits.append(fname, self._err_ghost_ratio)
+        fits.append(fname, self._init_coeff)
